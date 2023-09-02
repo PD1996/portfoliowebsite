9cad203a376752860bf9038a14b31e2cbb409cad
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-09-02 11:59:04 -0700

    Refactor CSS to prepare it for eventual dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import TypingEffect from "./components/TypingEffect";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="site-container">
+      <header className="main-header">
         <TypingEffect text="Hello, I'm Peter! 👋 Welcome to my portfolio website." />
         <TypingEffect text="Software Developer | Tech Enthusiast | Lifelong Learner" />
       </header>
