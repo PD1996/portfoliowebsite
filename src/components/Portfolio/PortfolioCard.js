2ea349ad73fb8bf80128887923ab2e853c79bed4
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-10-09 21:27:51 -0700

    Refactor portfolio cards to be their own component

---
+++ src/components/Portfolio/PortfolioCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+
+function PortfolioCard({
+  title,
+  link,
+  children,
+  cardNumber,
+  expandedCard,
+  setExpandedCard,
+}) {
+  return (
+    <div
+      className={`portfolio-card ${
+        expandedCard === cardNumber ? "card-expanded" : ""
+      }`}
+      onClick={() => {
+        if (expandedCard === cardNumber) {
+          setExpandedCard(null);
+        } else {
+          setExpandedCard(cardNumber);
+        }
+      }}
+    >
+      <h3>
+        {title}
+        <span className="toggle-arrow">
+          {expandedCard === cardNumber ? "▲" : "▼"}
+        </span>
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card-link-icon"
+        >
+          <FontAwesomeIcon icon={faExternalLinkAlt} />
+        </a>
+      </h3>
+      <div className="card-content">{children}</div>
+    </div>
+  );
+}
+
+export default PortfolioCard;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,18 +1,9 @@
 import React, { useState } from "react";
 import "./Portfolio.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import PortfolioCard from "./PortfolioCard";
 
 function Portfolio() {
   const [expandedCard, setExpandedCard] = useState(null);
-
-  const toggleCard = (cardIndex) => {
-    if (expandedCard === cardIndex) {
-      setExpandedCard(null);
-    } else {
-      setExpandedCard(cardIndex);
-    }
-  };
 
   return (
     <div id="portfolio">
@@ -22,88 +13,40 @@ function Portfolio() {
         working on) in my spare time!
       </p>
       <div className="portfolio-container">
-        <div
-          className={`portfolio-card ${
-            expandedCard === 1 ? "card-expanded" : ""
-          }`}
-          onClick={() => toggleCard(1)}
+        <PortfolioCard
+          title="Project 1"
+          link="https://link-to-project1.com"
+          cardNumber={1}
+          expandedCard={expandedCard}
+          setExpandedCard={setExpandedCard}
         >
-          <h3>
-            Project 1
-            <span className="toggle-arrow">
-              {expandedCard === 1 ? "▲" : "▼"}
-            </span>
-            <a
-              href="https://link-to-project1.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="card-link-icon"
-            >
-              <FontAwesomeIcon icon={faExternalLinkAlt} />
-            </a>
-          </h3>
-          <div className="card-content">
-            <p>Short description of project 1.</p>
-          </div>
-        </div>
+          <p>Short description of project 1.</p>
+        </PortfolioCard>
 
-        <div
-          className={`portfolio-card ${
-            expandedCard === 2 ? "card-expanded" : ""
-          }`}
-          onClick={() => toggleCard(2)}
+        <PortfolioCard
+          title="Project 2"
+          link="https://link-to-project2.com"
+          cardNumber={2}
+          expandedCard={expandedCard}
+          setExpandedCard={setExpandedCard}
         >
-          <h3>
-            Project 2
-            <span className="toggle-arrow">
-              {expandedCard === 2 ? "▲" : "▼"}
-            </span>
-            <a
-              href="https://link-to-project2.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="card-link-icon"
-            >
-              <FontAwesomeIcon icon={faExternalLinkAlt} />
-            </a>
-          </h3>
-          <div className="card-content">
-            <p>Short description of project 2.</p>
-          </div>
-        </div>
+          <p>Short description of project 2.</p>
+        </PortfolioCard>
 
-        <div
-          className={`portfolio-card ${
-            expandedCard === 3 ? "card-expanded" : ""
-          }`}
-          onClick={() => toggleCard(3)}
+        <PortfolioCard
+          title="Pythagorean Visualization Tool"
+          link="https://github.com/PD1996/.NET-Pythagorean-Theorem-Visualization-Tool"
+          cardNumber={3}
+          expandedCard={expandedCard}
+          setExpandedCard={setExpandedCard}
         >
-          <h3>
-            Pythagorean Visualization Tool
-            <span className="toggle-arrow">
-              {expandedCard === 3 ? "▲" : "▼"}
-            </span>
-            <a
-              href="https://github.com/PD1996/.NET-Pythagorean-Theorem-Visualization-Tool"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="card-link-icon"
-            >
-              <FontAwesomeIcon icon={faExternalLinkAlt} />
-            </a>
-          </h3>
-          <div className="card-content">
-            <img
-              src="pythagorean.png"
-              alt="Pythagorean Theorem Visualization"
-            />
-            <p>
-              A simple Windows desktop application used to visualize the
-              Pythagorean Theorem (A² + B² = C²).
-            </p>
-            <h5>March, 2019</h5>
-          </div>
-        </div>
+          <img src="pythagorean.png" alt="Pythagorean Theorem Visualization" />
+          <p>
+            A simple Windows desktop application used to visualize the
+            Pythagorean Theorem (A² + B² = C²).
+          </p>
+          <h5>March, 2019</h5>
+        </PortfolioCard>
       </div>
     </div>
   );
