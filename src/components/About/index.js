5dd94089063e24acffd01dac35da8988ef979f2c
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-10-09 18:56:53 -0700

    Simplify introduction text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,19 +16,11 @@ function About() {
             tag: "h2",
           },
           {
-            text: "From the heart of Kelowna, BC, Canada, my journey in tech has been driven by a desire to solve real-world challenges.",
-            tag: "p",
+            text: "A full-stack software developer with a passion for utilizing creativity to solve real-world problems.",
+            tag: "h3",
           },
           {
-            text: "While I gravitate towards backend development and its structured logic, I'm also skilled at creating functional and aesthetic user interfaces.",
-            tag: "p",
-          },
-          {
-            text: "Though rooted in BC, I'm always up for new adventures. Relocating? Definitely on the table for the right opportunity.",
-            tag: "p",
-          },
-          {
-            text: "Got a tech thought or just want to discuss the latest trends? I'm here for it. Let's connect!",
+            text: "I like to keep myself open to new opportunities and challenges, so please feel free to connect!",
             tag: "p",
           },
         ]}
