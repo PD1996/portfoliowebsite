02daf4fcf9b20e4abe4c22dd8c2c90eedeaca297
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-17 12:03:38 -0800

    Load Earth model

--- src/components/EarthModel/index.jsx
+++ src/components/EarthModel/index.jsx
@@ -1,10 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 
+import Earth from "./Earth";
+
 function EarthModel() {
   return (
     <section>
       <Canvas camera={{ near: 0.1, far: 1000 }}>
         <directionalLight color={0xfff6e5} intensity={1.5} />
+        <Earth position={[0, 0, -5]} />
       </Canvas>
     </section>
   );
