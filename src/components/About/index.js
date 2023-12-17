1a794e499bca02fb1271e97c59054a1f8627b8ec
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-17 10:35:07 -0800

    Add empty EarthModel component to About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+
 import TypingEffect from "../TypingEffect";
+import EarthModel from "../EarthModel";
+
 import "./About.css";
 
 function About() {
   return (
     <div className="about-container">
+      <EarthModel />
       <TypingEffect
         texts={[
           {
---
+++ src/components/EarthModel/index.js
@@ -0,0 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+
+function EarthModel() {
+  return (
+    <section>
+      <Canvas camera={{ near: 0.1, far: 1000 }}>
+        <directionalLight color={0xfff6e5} intensity={1.5} />
+      </Canvas>
+    </section>
+  );
+}
+
+export default EarthModel;
