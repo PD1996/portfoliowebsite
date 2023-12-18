9ed7e4ef9dabaa59dda430f6e6e56b6625f5b0f4
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-17 18:07:45 -0800

    Update dark mode default value

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   useEffect(() => {
     const localDarkMode = localStorage.getItem("darkMode") === "true";
