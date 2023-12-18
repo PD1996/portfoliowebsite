09950e449b043b587a2139f680caa5ab4ffc53c3
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-17 17:04:13 -0800

    Remove centering logic for now

--- src/components/EarthModel/Earth.jsx
+++ src/components/EarthModel/Earth.jsx
@@ -6,7 +6,7 @@ Source: https://sketchfab.com/3d-models/earth-63d902b12fd14868b4dc2f19dc21d7c2
 Title: Earth
 */
 
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
@@ -27,15 +27,6 @@ function Earth(props) {
     const elapsedTime = clock.getElapsedTime();
     earthRef.current.rotation.y = elapsedTime / 10;
   });
-
-  useEffect(() => {
-    if (earthRef.current) {
-      const box = new THREE.Box3().setFromObject(earthRef.current);
-      const center = box.getCenter(new THREE.Vector3());
-      console.log(center);
-      earthRef.current.position.sub(center);
-    }
-  }, []);
 
   return (
     <a.group ref={earthRef} scale={scale} {...props}>
