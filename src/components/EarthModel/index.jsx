1c9c3dacca42e5d8353b050e2f507367628e53d4
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-17 18:08:40 -0800

    Update EarthModel camera position and autoRotateSpeed

--- src/components/EarthModel/index.jsx
+++ src/components/EarthModel/index.jsx
@@ -8,18 +8,24 @@ function EarthModel() {
   return (
     <section className="earth-container">
       <Canvas
-        camera={{ position: [5, 3, 0.1], near: 0.1, far: 1000, fov: 45 }}
+        camera={{
+          position: [5, 3, 3],
+          up: [0, 0, 1],
+          near: 0.1,
+          far: 1000,
+          fov: 45,
+        }}
         pixelratio={2}
       >
         <directionalLight
           color={0xfff6e5}
           intensity={1.5}
-          position={(0, 10, 10)}
+          position={(0, 0, 10)}
         />
         <Earth position={[0, 0, 0]} />
         <OrbitControls
           autoRotate
-          autoRotateSpeed={2.5}
+          autoRotateSpeed={3}
           enableDamping
           enablePan={false}
           enableZoom={false}
