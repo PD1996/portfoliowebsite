8eb8045b70c6aa475c2b2d3b7beb8cd1a1702a6b
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-21 04:39:10 -0800

    Swap order of LinkedIn/GitHub links

--- src/components/Navbar/IconLinks.jsx
+++ src/components/Navbar/IconLinks.jsx
@@ -8,26 +8,19 @@ function IconLinks({ className }) {
   return (
     <div className={`icon-links ${className}`}>
       <a
-        href="https://github.com/PD1996"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FontAwesomeIcon icon={faGithub} className="nav-icon github" />
-      </a>
-      <a
         href="https://www.linkedin.com/in/peter-dyczkowski-5a653725a"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FontAwesomeIcon icon={faLinkedin} className="nav-icon linkedin" />
       </a>
-      {/* <a
-        href="Peter Dyczkowski Resume.pdf"
+      <a
+        href="https://github.com/PD1996"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FontAwesomeIcon icon={faFilePdf} className="nav-icon pdf" />
-      </a> */}
+        <FontAwesomeIcon icon={faGithub} className="nav-icon github" />
+      </a>
     </div>
   );
 }
