934422d6ee1bba87c12efb0e7ffd6492ae9675cf
Peter Dyczkowski <48145220+PD1996@users.noreply.github.com>
2023-12-28 13:38:12 -0800

    Replace chess bot project with custom trading client

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -11,14 +11,11 @@ function Portfolio() {
     <div id="portfolio">
       <h1>Portfolio</h1>
       <div className="text-container">
-        <p>
-          A few personal projects I've worked on (or am currently working on) in
-          my spare time
-        </p>
+        <p>A few personal projects I've worked on in my spare time.</p>
       </div>
       <div className="portfolio-container">
         <PortfolioCard
-          title="This Website!"
+          title="This Website"
           link="https://github.com/PD1996/portfoliowebsite/tree/main/src"
           cardNumber={1}
           expandedCard={expandedCard}
@@ -32,7 +29,7 @@ function Portfolio() {
           <h5>2023</h5>
         </PortfolioCard>
 
-        <PortfolioCard
+        {/* <PortfolioCard
           title="AI Chess Bots"
           link="https://github.com/PD1996/ai-chess-bot"
           cardNumber={2}
@@ -44,7 +41,27 @@ function Portfolio() {
             Will contain multiple chess engines trained in different ways for
             experimental purposes.
           </p>
-          <h5>October, 2023 - In Progress</h5>
+          <h5>In Progress</h5>
+        </PortfolioCard> */}
+
+        <PortfolioCard
+          title="Custom Trading Client"
+          cardNumber={2}
+          expandedCard={expandedCard}
+          setExpandedCard={setExpandedCard}
+        >
+          <img src="tradingclient.gif" alt="Trading Client Demo" />
+          <p>
+            A Windows-based futures trading client built with C#. Executes
+            orders and fetches market data through Interactive Brokers' TWS API.
+            Automatically stores and retrieves analytics to and from an SQL
+            database. Loads tradingview.com in a .NET webView for seemless
+            charting access. Source code has been made private to prevent the
+            possibility of somebody incurring financial losses while using this
+            software. That being said, I would be happy to provide more details
+            to those who reach out!
+          </p>
+          <h5>November, 2022</h5>
         </PortfolioCard>
 
         <PortfolioCard
@@ -56,8 +73,8 @@ function Portfolio() {
         >
           <img src="pythagorean.gif" alt="Pythagorean Theorem Visualization" />
           <p>
-            A Windows desktop application built with C# and .NET used to
-            visualize the Pythagorean Theorem (A² + B² = C²).
+            A Windows desktop application built with C# used to visualize the
+            Pythagorean Theorem (A² + B² = C²).
           </p>
           <h5>March, 2019</h5>
         </PortfolioCard>
